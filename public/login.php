9e8ff38bbd96c98bd5bfc912f9d6760130ca19ea
AmantiniDavide <davide.amantini@studio.unibo.it>
2026-01-20 14:22:30 +0100

    login/register page done

--- public/login.php
+++ public/login.php
@@ -1,9 +1,22 @@
-<?php require_once '../includes/db.php';
-include '../includes/header.php'; ?>
-<h2>Accedi a CampusLaunch</h2>
-<form action="../actions/login_action.php" method="POST">
-    <input type="email" name="email" placeholder="Email" required><br><br>
-    <input type="password" name="password" placeholder="Password" required><br><br>
-    <button type="submit">Entra</button>
-</form>
+<?php 
+require_once '../includes/db.php';
+include '../includes/header.php'; 
+?>
+
+<div class="container">
+    <h2 class="section-title">Accedi a CampusLaunch</h2>
+
+    <form action="../actions/login_action.php" method="POST" class="auth-form">
+        <input type="email" name="email" placeholder="Email" required class="auth-input">
+        <input type="password" name="password" placeholder="Password" required class="auth-input">
+        
+        <button type="submit" class="btn-full-width">Entra</button>
+    </form>
+
+    <div class="auth-footer">
+        <p>Non hai ancora un account?</p>
+        <a href="register.php" class="link-highlight">Crea un nuovo account</a>
+    </div>
+</div>
+
 <?php include '../includes/footer.php'; ?>

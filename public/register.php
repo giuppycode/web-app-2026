f68689763db20a5615c987e40d9866d896836df4
Giuseppe Cattolico <giuseppe.cattolico@studio.unibo.it>
2026-01-22 21:17:49 +0100

    remove icon

--- public/register.php
+++ public/register.php
@@ -5,12 +5,12 @@ include '../includes/header.php'; ?>
 
     <?php if (isset($_GET['error'])): ?>
         <div class="error-alert">
-            ⚠️ <?= htmlspecialchars($_GET['error']) ?>
+            <?= htmlspecialchars($_GET['error']) ?>
         </div>
     <?php endif; ?>
 
     <h2 class="section-title">Unisciti alla Community</h2>
-    
+
     <form action="../actions/register_action.php" method="POST" class="auth-form">
         <div class="row-2">
             <input type="text" name="firstname" placeholder="First Name" required class="auth-input">
@@ -19,15 +19,16 @@ include '../includes/header.php'; ?>
 
         <input type="email" name="email" placeholder="Email universitaria" required class="auth-input">
         <input type="password" name="password" placeholder="Password" required class="auth-input">
-       
+
         <input type="text" name="username" placeholder="Username" required class="auth-input">
         <input type="text" name="number" placeholder="Phone Number" required class="auth-input">
         <input type="text" name="faculty" placeholder="Faculty" required class="auth-input">
-        
-        
-        
-        <textarea name="biography" placeholder="Raccontaci brevemente chi sei..." class="auth-input" rows="3"></textarea>
-        
+
+
+
+        <textarea name="biography" placeholder="Raccontaci brevemente chi sei..." class="auth-input"
+            rows="3"></textarea>
+
         <button type="submit" class="btn-full-width">Registrati</button>
     </form>
 
